b5215a94445fd790558772761a93bbfe29a2d9ab
echlaihi <abdelghaniechlaihi514@gmail.com>
2024-06-10 19:41:12 +0100

    modifying the migration of reservations, partners

--- database/migrations/2024_06_10_183828_create_reservations_table.php
+++ database/migrations/2024_06_10_183828_create_reservations_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('reservations', function (Blueprint $table) {
             $table->id();
             $table->foreignId("user_id")->references("id")->on("users");
+            $table->foreignId("partner_id")->references("id")->on("partners");
             $table->foreignId("room_id")->references("id")->on("rooms");
             $table->string("status"); // active, ready, disactive, terminate
             $table->date("start_date");
