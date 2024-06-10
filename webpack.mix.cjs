3defe8209c1010afee88c66937d3100cef018d00
echlaihi <abdelghaniechlaihi514@gmail.com>
2024-06-10 19:42:16 +0100

    configuring webpack.mix

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js') // Concatenate and compile multiple JS files
+mix.js(['resources/js/app.js',], 'public/js/app.js') // Concatenate and compile multiple JS files
   .sass('resources/scss/main.scss', 'public/css') // Compile SCSS (optional)
   .sass('resources/admin/scss/style.scss', 'public/css'); // Compile SCSS (optional)
